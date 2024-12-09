54cb353a0ab9d359e63bff85e24d9c3ef625b579
Satoru Komatsu <skomatsu@c4r2s13.42tokyo.jp>
2024-12-09 22:02:04 +0900

    main

--- command.c
+++ command.c
@@ -1,1 +1,5 @@
+#include "push_swap.h"
 
+void swap(t_list stack)
+{
+    int temp;
---
+++ main.c
@@ -0,0 +1,21 @@
+include "push_swap.h"
+
+int main(int argc, char *argv[])
+{
+    t_list stack_a;
+    t_list stack_b;
+    int i;
+    
+    if(argc < 2)
+    {
+        ft_printf();
+        return (NULL);
+    }
+    ini_addstack(&stack_a,argc, argv);
+
+}
+
+void    ini_addstack(t_list *stack_a, int argc, char *argv[])
+{
+    
+}
